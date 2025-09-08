ae34dc7a5e6fdd17019c5550dec886522a3c44e3
ELI-1000-dev <ST10494408@rcconnect.edu.za>
2025-09-08 07:30:20 +0000

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,54 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Latest Series-2025");
+        System.out.println("************************************************");
+
+        while (running) {
+            System.out.println("\nEnter (1) to launch menu or any other key to exit");
+            String input = scanner.nextLine();
+
+            if (!input.equals("1")) {
+                System.out.println("Goodbye!");
+                break;
+            }
+            System.out.println("\nPlease select one of the following menu items:");
+            System.out.println("(1) Capture a new series.");
+            System.out.println("(2) Search for a series.");
+            System.out.println("(3) Update series age restriction.");
+            System.out.println("(4) Delete a series.");
+            System.out.println("(5) Print series report - 2025");
+            System.out.println("(6) Exit Application.");
+
+            String choice = scanner.nextLine();
+
+            switch (choice) {
+                case "1":
+                    Series.captureSeries(scanner);
+                    break;
+                case "2":
+                    Series.searchSeries(scanner);
+                    break;
+                case "3":
+                    Series.updateSeries(scanner);
+                    break;
+                case "4":
+                    Series.deleteSeries(scanner);
+                    break;
+                case "5":
+                    Series.printReport();
+                    break;
+                case "6":
+                    Series.exitSeriesApplication();
+                    break;
+                default:
+                    System.out.println("Invalid choice.");
+            }
         }
     }
 }
+
